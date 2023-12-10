97f5754fe6507bf61e2726aff57864eca22eeb6d
trent.wyatt <trent@trentwyatt.com>
2023-12-10 15:19:58 -0600

    Update CPUVolt.ino
    Added example of turning on a "needs charging" LED if desired

--- examples/CPUVolt/CPUVolt.ino
+++ examples/CPUVolt/CPUVolt.ino
@@ -10,27 +10,43 @@
 void setup() {
     Serial.begin(115200);
 
-    // Read the current voltage level and convert it
-    // to an easier to read floating point value
+    /*
+     * Read the current voltage level and convert it
+     * to an easier to read floating point value
+     */
     float mv = readVcc() / 1000.0;
 
     // Show the voltage with 2 decimal places
     Serial.print("Voltage: ");
     Serial.println(mv, 2);
 
-    // Get the voltage level as a percentage of total charge.
-    // You can optionally specify the voltage level to be considered
-    // as 100%. The default voltage is 5V if not supplied.
+    /*
+     * Get the voltage level as a percentage of total charge.
+     * You can optionally specify the voltage level to be considered
+     * as 100%. The default voltage is 5V if not supplied.
+     */
     float pct = readPercent(/*5000*/);
     Serial.print("Percent: ");
     Serial.println(pct, 2);
 
-    // You can also specify both the lower and upper voltage
-    // ranges to be considered what is 0% and what is 100%.
-    // This is really useful for battery powered projects!
+    /*
+     * You can also specify both the lower and upper voltage
+     * ranges to be considered what is 0% and what is 100%.
+     * This is really useful for battery powered projects!
+     */
     pct = readPercent(2900, 4700);
     Serial.print("Percent: ");
     Serial.println(pct, 2);
+
+    /*
+     * The following code shows how you might use the percent of charge
+     * to turn on a "needs recharging" LED at the percent you want:
+     */
+    float const low_pct = 70.0;                // season to taste :)
+    bool const needs_charging = pct <= low_pct;// set a "needs charging" flag
+    int const led_pin = LED_BUILTIN;           // Or use any other GPIO pin (besides A6 or A7)
+    pinMode(led_pin, OUTPUT);                  // set the pin as an output
+    digitalWrite(led_pin, needs_charging);     // set the LED  pin HIGH or LOW as needed
 }
 
 void loop() { }
